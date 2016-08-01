4da70a067381c2cf9e3a90faaeb7aeb01e063121
jaspercurry <jaspercurry@gmail.com>
2016-07-31 21:21:42 -0400

    made inheritance class

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative './user.rb'
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def knowledge
+    @knowledge
+  end
+
+  def learn(know_stuff)
+    self.knowledge << know_stuff
+  end
+
 
 end
